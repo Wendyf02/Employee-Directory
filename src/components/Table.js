f04227575cec00c004954c1e6d3a159919a6bf41
Wendy Fgigueroa <camomila444@gmail>
2021-03-26 21:48:18 -0400

    working on table

--- src/components/Table.js
+++ src/components/Table.js
@@ -29,15 +29,117 @@ class Table extends React.Component {
          search: searchTerm
 
       })
-
     }
   }
 
+  // sorted by first name
+  sortByName = () => {
+     const sortedEmployees = this.state.results.sort((a,b) => {
+        if (b.name.first > a.name.first) {
+          return -1
+        }
+        if (a.name.first > b.name.first) {
+          return 1
+        }
+          return 0;
+
+     });
+
+     if (this.state.sortOrder === "DESC" ) {
+        sortedEmployees.reverse();
+        this.setState({ sortOrder: "ASC "});
+     } else {
+         this>this.setState({ sortOrder: "DESC" });
+     }
+       this.setState({ results: sortedEmployees })
+
+  }
+
+  // sorted by last name
+  sortByName = () => {
+    const sortedEmployees = this.state.results.sort((a,b) => {
+       if (b.name.last > a.name.last ) {
+         return -1
+       }
+       if (a.name.last > b.name.last ) {
+         return 1
+       }
+         return 0;
+
+    });
+
+    if (this.state.sortOrder === "DESC" ) {
+       sortedEmployees.reverse();
+       this.setState({ sortOrder: "ASC "});
+    } else {
+        this>this.setState({ sortOrder: "DESC" });
+    }
+      this.setState({ results: sortedEmployees })
+
+ }
+
+
+// render items
+
+  render() {
+
+    <div>
+
+      <Search handleInputchange={this.handleInputchange}
+        search={this.state.search} />
+
+      <div classNmae="table-responsive">   
+        <table className="table table-striped table-responsive  text-center table-hover"> 
+      
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>First className <span className="downArrow" onClick={this.sortByName}></span></th>
+              <th>Lastr Name <span className="downArrow" onClick={this.sortByName}></span></th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>DOB</th>
+            </tr>
+          </thead>
+      
+      
+      
+      
+     
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </table>
+    
+      </div>
+    </div>
+
+  }
 
   
 }
